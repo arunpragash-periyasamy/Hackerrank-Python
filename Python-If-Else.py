261787a3e7df99b32f06df0b44a8e1ed876ff007
arunpragash-annanperiasamy <79126203+arunpragash-periyasamy@users.noreply.github.com>
2022-03-08 22:33:16 +0530

    Create Python-If-Else.py
    hackerrank python if else answer

---
+++ Python-If-Else.py
@@ -0,0 +1,10 @@
+a=int(input())
+if(a%2==0):
+    if(a>=2 and a<5):
+        print ("Not Weird")
+    elif(a<=20):
+        print("Weird")
+    else:
+        print("Not Weird")
+else:
+    print("Weird") 
